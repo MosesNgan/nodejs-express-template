808f1c0f9e2720730fc7ebcf8444de7d38684187
Moses Ngan <mosesngan@Mosess-MacBook-Pro.local>
2023-05-22 11:17:59 +0800

    change all .js to .ts; update package.json and readme.md

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = new express();
-app.use('/', (req, res) => {
-  res.send('Working');
-});
-
-module.exports = app;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const { env, port } = require('./config/config');
-
-app.listen(port, () => {
-  console.log(`[${env}] Listening to port ${port}`);
-});
