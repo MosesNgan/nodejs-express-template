5810a1babdfda386a0e11d88224da92fda56af75
MosesNgan <mngan@palo-it.com>
2023-05-24 16:14:47 +0800

    add redis usage example to the endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { getFromCache, setToCache } from './services/cache';
 
 const app = express();
 
-app.use('/', (req: Request, res: Response) => {
-  res.send('Working');
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const value = await getFromCache('homePageMessage');
+    const homePageMessage = 'Home page is working :)';
+    if ( value == homePageMessage) {
+      console.log('Retrieved data from Redis cache:', value);
+      return res.send(value);
+    } else {
+
+      await setToCache('homePageMessage', homePageMessage);
+      console.log('Set data in Redis cache:', homePageMessage);
+      return res.send(homePageMessage);
+    }
+  } catch (error) {
+    console.error('Error accessing Redis cache:', error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 export default app;
